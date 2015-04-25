3c220f8a8cb4c3ee08eb71751f74e09143aea28c
Sam <Names@carlwireless-185-16.dyn.carleton.edu>
2015-04-25 12:50:25 -0500

    made the status menu modular!

--- TabTable/AppDelegate.swift
+++ TabTable/AppDelegate.swift
@@ -10,43 +10,21 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    @IBOutlet var statusMenu: NSMenu?;
-    @IBOutlet weak var window: NSWindow!
-    var statusItem: NSStatusItem?;
-    var masterViewController: MasterViewController!
-
-    // Unknown functionality... good luck...
-    var button: NSStatusBarButton?
-    override func awakeFromNib() {
-        let NSVariableStatusItemLength: CGFloat = -1.0
-        
-        self.statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
-        self.button = self.statusItem?.button
-        self.button?.title = "Press Me"
-        self.button?.action = "pressed"
-        self.button?.target = self
-    }
     
-    func pressed(sender : AnyObject) {
-        println("Pressed")
-    }
+    @IBOutlet weak var window: NSWindow!
+    var masterViewController: MasterViewController!
+    var menuControl: MenuControl?
+
     
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         masterViewController = MasterViewController(nibName: "MasterViewController", bundle: nil)
         masterViewController.setupSampleTable()
-        
         window.contentView.addSubview(masterViewController.view)
         masterViewController.view.frame = (window.contentView as! NSView).bounds
-
-        // menu stuff
-        let bar = NSStatusBar.systemStatusBar()
-        let NSVariableStatusItemLength: CGFloat = -1.0
         
-        statusItem = bar.statusItemWithLength(CGFloat(NSVariableStatusItemLength))
-        statusItem!.title = "Tab Nab"
-        statusItem!.menu = statusMenu
-        statusItem!.highlightMode = true
+        self.menuControl = MenuControl()
+        self.menuControl?.statusMenuLaunch()
         // Insert code here to initialize your application
 
     }
---
+++ TabTable/MenuControl.swift
@@ -0,0 +1,30 @@
+//
+//  MenuControl.swift
+//  TabTable
+//
+//  Created by Sam on 4/25/15.
+//  Copyright (c) 2015 ebal. All rights reserved.
+//
+
+import Cocoa
+
+class MenuControl: NSMenu {
+    @IBOutlet var statusMenu: NSMenu! = NSMenu();
+    var statusItem: NSStatusItem?;
+    var menuItem: NSMenuItem = NSMenuItem()
+    
+    func statusMenuLaunch() {
+        // menu stuff
+        let bar = NSStatusBar.systemStatusBar()
+        let NSVariableStatusItemLength: CGFloat = -1.0
+        
+        statusItem = bar.statusItemWithLength(CGFloat(NSVariableStatusItemLength))
+        statusItem!.title = "Tab Nab"
+        statusItem!.menu = statusMenu
+        statusItem!.highlightMode = true
+        
+        menuItem.title = "close"
+        statusMenu.addItem(menuItem)
+
+    }
+}
